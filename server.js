39d2c0fff0b1c62328b32999ba6c44770957744c
Tehnikrangrej <irshadrangreh@gmail.com>
2026-01-28 12:34:27 +0530

    Change Port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require ("dotenv").config();
 
 const app = require ("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get ("/health", (req, res) => {
   res.status (200).send ("OK");
