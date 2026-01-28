e259b6f3866c86c4ea303152cd76de9bed8fdaa2
Tehnikrangrej <irshadrangreh@gmail.com>
2026-01-28 12:57:05 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express(); 
-
+const cors = require('cors');
 
 app.use(express.json());
 
+app.use(cors({
+  origin: ["http://localhost:8080"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
